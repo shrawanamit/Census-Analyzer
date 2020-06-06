2bfae744040926dffe6304e9dd8a18e14436ffef
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-06 15:16:20 +0530

    Added: UC1 check no of record in indian census CSV

---
+++ CensusAnalyser/CensusAnalyzerException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CensusAnalyser
+{
+    [Serializable]
+    internal class CensusAnalyzerException : Exception
+    {
+       
+
+        public enum ExceptionType
+        {
+            EMPTY_FILE, FILE_NOT_FOUND,
+            WRONG_DELIMETER, INVALID_CENSUS_DATA
+        }
+        public ExceptionType EType;
+
+        public CensusAnalyzerException(ExceptionType EType, string message) : base(message)
+        {
+            this.EType = EType;
+        }
+        
+
+       
+    }
+}
--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using CensusAnalyser;
+using System.Data;
 
 namespace NUnitTestProject1
 {
@@ -7,12 +9,19 @@ namespace NUnitTestProject1
         [SetUp]
         public void Setup()
         {
+            
         }
 
+        public static string CENSUS_CSV_FILE_PATH = @"G:\vs\CensusAnalyser\NUnitTestProject1\IndiaStateCensusData.csv";
+        public static string WRONG_CSV_FILE_PATH = @"IndiaStateCensusData.csv";
+
+        //cheaing no of records
         [Test]
-        public void Test1()
+        public void GivenCensusCSVFile_ShouldReturn_CorrectNumberOfRecords()
         {
-            Assert.Pass();
+            DataTable csvData = CensusAnalyserManager.LoadCensusData(CENSUS_CSV_FILE_PATH);
+            Assert.AreEqual(29, csvData.Rows.Count);
         }
+
     }
 }
